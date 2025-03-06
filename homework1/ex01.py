3734c76cb428bdc6ade8a35ffc8b0cde88bbb3ae
Hripsime <grigoryanhripsime18@gmail.com>
2025-03-06 20:05:43 +0400

    ex01 added

--- homework1/ex01.py
+++ homework1/ex01.py
@@ -1,12 +1,22 @@
-sum = 0
+sum1 = 0
 
 while True:
-    choice = int(input("Enter a number(1 or 2): "))
+    while True:
+        try:
+            choice = int(input("Enter a number(1 or 2): "))
+            break
+        except ValueError:
+            print("invalid input")
     if choice == 1:
-        num = int(input("Enter a number: "))
-        sum += num
+        while True:
+            try:
+                num = int(input("Enter a number: "))
+                break
+            except ValueError:
+                print("invalid input")
+        sum1 += num
     elif choice == 2:
-        print("sum = ", sum)
+        print("sum = ", sum1)
         break
     else:
         print("Invalid input!")
